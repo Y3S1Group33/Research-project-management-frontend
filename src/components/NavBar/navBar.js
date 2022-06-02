c9669eb4814b52481127ec3e18e0b8c0e48dfc53
uihalagedara <it20224820@my.sliit.lk>
2022-06-03 00:02:37 +0530

    nav bar updated

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -8,7 +8,7 @@ function NavBar() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand " href="/">
-            HMS
+            RPM
           </a>
 
           <button
