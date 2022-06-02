bda7e6962f557a32dfcac5a5fe4e16307a28d232
uihalagedara <it20224820@my.sliit.lk>
2022-06-02 18:43:21 +0530

    nav bar updated

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -23,6 +23,7 @@ function NavBar() {
             <span className="navbar-toggler-icon"></span>
           </button>
 
+          {/* panel member */}
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
