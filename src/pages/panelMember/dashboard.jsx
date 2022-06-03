83898272c7adfa0ce35895a4ed3999f4c0ece257
uihalagedara <it20224820@my.sliit.lk>
2022-06-03 14:34:30 +0530

    dashboard initial cards added

--- src/pages/panelMember/dashboard.jsx
+++ src/pages/panelMember/dashboard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import presentationIMG from '../../../public/images/presentations.webp'
 import researchIMG from '../../../public/images/reseachs.webp'
 import submissionsIMG from '../../../public/images/submission.webp'
 
 export default function dashboard() {
+
+  const [submissionDetails, setSubmissionDetails] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://floating-meadow-01028.herokuapp.com/api/makeSubmission`)
+      .then((response) => response.json())
+      .then((responseData) => {
+        setSubmissionDetails(responseData);
+      });
+    console.log(submissionDetails);
+  }, []);
+
   return (
     <div class="container" style={{ marginTop:"20px"}}>
     <div class="row row-cols-1 row-cols-md-3 g-4">
@@ -30,7 +42,7 @@ export default function dashboard() {
       <img src={submissionsIMG} class="card-img-top" alt="..." />
       <div class="card-body">
         <h5 class="card-title">Submissions</h5>
-        <p class="card-text">Submissions card body</p>
+        <p class="card-text">No of submissions - {submissionDetails.length}</p>
       </div>
     </div>
   </div>
