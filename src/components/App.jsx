9c40963cebf20ececefb7d8295e0064498518c39
danushka9999 <dranasinghe99@gmail.com>
2022-06-03 12:11:01 +0530

    front end routing finalized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,16 @@ import PanelMemberDashboard from '../pages/panelMember/dashboard'
 import PanelMemberChat from '../pages/panelMember/chat'
 import PresentationEvaluate from '../pages/panelMember/presentationEvaluate'
 
+
+//supervisor co supervisor
+import SupervisorDashboard from "../pages/supervisor/supervisorDashboard";
+import SupervisorTeams from "../pages/supervisor/supervisorTeams";
+import SupervisorMarkingSchemes from "../pages/supervisor/supervisorMarkingSchemes";
+import TeamChat from "../pages/supervisor/teamChat";
+import TopicsConfirmation from "../pages/supervisor/topicsConfirmation";
+import SupervisorDocuments from "../pages/supervisor/supervisorDocuments";
+
+
 export default function App() {
   return (
     <div>
@@ -18,18 +28,20 @@ export default function App() {
 
       <Router>
               <Routes>
-                  //Panel member routes
+                  {/*Panel member routes*/}
               <Route exact path="/panelMember/topics" element={<TopicsEvaluate />} />
               <Route exact path="/panelMember/reference" element={<Reference />} />
               <Route exact path="/panelMember/dashboard" element={<PanelMemberDashboard />} />
               <Route exact path="/panelMember/chat" element={<PanelMemberChat />} />
-               <Route exact path="/panelMember/presentation" element={<PresentationEvaluate />} />
+              <Route exact path="/panelMember/presentation" element={<PresentationEvaluate />} />
 
-                  //Supervisor/Co Supervisor routes
+                  {/*Supervisor-Co Supervisor routes*/}
               <Route exact path="/supervisor/dashboard" element={<SupervisorDashboard />} />
-              <Route exact path="/supervisor/topics" element={<SupervisorTopicsEvaluate />} />
-              <Route exact path="/supervisor/teams" element={<Teams />} />
-              <Route exact path="/supervisor/documentEvaluation" element={<DocumentEvaluation />} />
+              <Route exact path="/supervisor/teams" element={<SupervisorTeams />} />
+              <Route exact path="/supervisor/topics" element={<TopicsConfirmation />} />
+              <Route exact path="/supervisor/document" element={<SupervisorDocuments />} />
+              <Route exact path="/supervisor/markingSchemes" element={<SupervisorMarkingSchemes />} />
+              <Route exact path="/supervisor/teams/chat/:id" element={<TeamChat />} />
 
 
               </Routes>
