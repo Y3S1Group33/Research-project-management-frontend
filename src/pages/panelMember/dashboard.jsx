d4d6e2b9d04b782b757d93fe416daa6557d8bdfd
uihalagedara <it20224820@my.sliit.lk>
2022-06-04 12:05:56 +0530

    dashboard completed

--- src/pages/panelMember/dashboard.jsx
+++ src/pages/panelMember/dashboard.jsx
@@ -103,7 +103,7 @@ export default function dashboard() {
                   <div class="modal-content">
                     <div class="modal-header">
                       <h5 class="modal-title" id="exampleModalLongTitle">
-                        Assigned Research Groups
+                        Uploaded references
                       </h5>
                       <button
                         type="button"
